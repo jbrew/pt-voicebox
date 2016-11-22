713c0d69dc18384f0ce691cfdeb3aca3a3199dff
pennetti <tjp7mb@virginia.edu>
2016-11-21 20:01:24 -0600

    Update setup.py to include dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
 setup(name='voicebox',
-      version='0.1',
+      version='0.2',
       description='Predictive text writing tool',
       url='',
       author='Jamie Brew',
       license='',
+      install_requires=[
+            'nose==1.3.7',
+            'six==1.10.0'
+      ],
       packages=['voicebox'])
